0ae7bcf28bc3dd5102b5a0a6fd06e67aaf938c74
Yett Shiuong Lu <yettshiuonglu@yetts-imac.local>
2017-07-10 15:32:31 +0800

    Initial Commit

---
+++ JetFirst/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JetFirst
+//
+//  Created by Yett Shiuong Lu on 10/07/2017.
+//  Copyright © 2017 Yett Shiuong Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
